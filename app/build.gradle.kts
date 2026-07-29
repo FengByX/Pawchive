097ce39fe0eb74140b56618d7476eca56bb1f69b
Pawchive Builder <builder@pawchive.pw>
2026-07-29 13:48:27 +0800

    release: v1.2.4 - optimize sort options, bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pawchive"
         minSdk = 30
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.2.3"
+        versionCode = 21
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
