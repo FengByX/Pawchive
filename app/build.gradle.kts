6deb4f97b741afaa66dc7400eb241b9766f184a8
FengByX <FengByX@users.noreply.github.com>
2026-08-01 11:03:28 +0800

    chore: bump version to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pawchive"
         minSdk = 30
         targetSdk = 36
-        versionCode = 39
-        versionName = "1.4.3"
+        versionCode = 40
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
